f8870e45139070394ea9f3813058364e4287d67f
JFL110 <typus24@hotmail.com>
2021-03-03 19:26:34 +0000

    Test logging, github actions

---
+++ src/test/java/jamesleach/dev/docsync/TestNothing.java
@@ -0,0 +1,16 @@
+package jamesleach.dev.docsync;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Empty test to verify the build works.
+ */
+class TestNothing {
+  @Test
+  void testNothing() {
+    assertEquals(4, 2 + 2);
+  }
+}
